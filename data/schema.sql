458c83921e5692e6f46a04ae3d7f785adac4e10a
David Persson <davidpersson@gmx.de>
2014-09-10 09:54:24 +0200

    Adding schema.

---
+++ data/schema.sql
@@ -0,0 +1,10 @@
+CREATE TABLE `tags` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(250) NOT NULL DEFAULT '',
+  `title` varchar(250) DEFAULT '',
+  `description` text,
+  `is_published` tinyint(1) unsigned NOT NULL DEFAULT '0',
+  `created` datetime NOT NULL,
+  `modified` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
