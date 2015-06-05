3ce4abb81a096672fe83fed7cdb81a538f1d2917
David Persson <davidpersson@gmx.de>
2015-06-05 14:43:50 +0200

    Use new editor helper.

--- views/tags/admin_form.html.php
+++ views/tags/admin_form.html.php
@@ -33,10 +33,10 @@ $this->set([
 		</div>
 		<div class="grid-row">
 			<div class="grid-column-left">
-				<?= $this->form->field('description', [
-					'type' => 'textarea',
+				<?= $this->editor->field('description', [
 					'label' => $t('Description'),
-					'wrap' => ['class' => 'body use-editor editor-basic editor-link']
+					'size' => 'beta',
+					'features' => 'minimal'
 				]) ?>
 			</div>
 			<div class="grid-column-right">
